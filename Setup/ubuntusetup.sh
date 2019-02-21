47fd166cb367edf8723fcf07af4549ef89948644
lbrame <luca.brame.lb@gmail.com>
2019-02-21 17:52:24 +0100

    Corrected typo in the comments of ubuntusetup.sh

--- Setup/ubuntusetup.sh
+++ Setup/ubuntusetup.sh
@@ -38,7 +38,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 
-# Refreshing pacakge list
+# Refreshing package list
 apt update -y
 
 echo "Installing programs..."
