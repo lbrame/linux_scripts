0560bacd4b485a431fb6068c4e168fbe137d95f6
lbrame <luca.brame.lb@gmail.com>
2019-05-15 22:13:12 +0200

    darklight - now switches between dark and light icon theme

--- Linux/Usability/dark_light_mode/dark.sh
+++ Linux/Usability/dark_light_mode/dark.sh
@@ -2,5 +2,6 @@
 
 gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark-compact'
 gsettings set org.gnome.shell.extensions.user-theme name 'Materia-dark-compact'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 gsettings set org.gnome.desktop.background picture-uri "/home/luca/Pictures/Wallpapers/darklight_wp/dark.jpg"
 
--- Linux/Usability/dark_light_mode/light.sh
+++ Linux/Usability/dark_light_mode/light.sh
@@ -2,4 +2,5 @@
 
 gsettings set org.gnome.desktop.interface gtk-theme 'Materia-compact'
 gsettings set org.gnome.shell.extensions.user-theme name 'Materia-compact'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
 gsettings set org.gnome.desktop.background picture-uri "/home/luca/Pictures/Wallpapers/darklight_wp/light.png"
